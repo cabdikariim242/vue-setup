4f90bf33650e576f6bc7e71217223ae59a706531
Abdirahmaan12 <abdirahmaanmoha2022@gmail.com>
2025-08-11 12:53:25 +0300

    added realy date picker

--- src/views/RadioButton.vue
+++ src/views/RadioButton.vue
@@ -21,6 +21,7 @@
             label="Chris Evans"
             value="Chris Evans"
             v-model="selectedVertical"
+            :customclass="'w-[18px] h-[18px] rounded-full inline-block transition-all duration-200 ease-in-out'"
           />
 
           <RadioButton
@@ -28,12 +29,25 @@
             label="Liam Neeson"
             value="Liam Neeson"
             v-model="selectedVertical"
+            :customclass="'w-[18px] h-[18px] rounded-full inline-block transition-all duration-200 ease-in-out'"
+
           />
           <RadioButton
             name="paymentVertical"
             label="Daniel Craig"
             value="Daniel Craig"
             v-model="selectedVertical"
+            :customclass="'w-[18px] h-[18px] rounded-full inline-block transition-all duration-200 ease-in-out'"
+
+          />
+          <RadioButton
+            name="paymentVertical"
+            label="Samafale"
+            value="Samafale"
+            v-model="selectedVertical"
+            :disabled="true"
+            :customclass="'cursor-not-allowed w-[18px] h-[18px] rounded-full inline-block transition-all duration-200 ease-in-out'"
+
           />
 
 
@@ -46,6 +60,8 @@
               label="Chris Evans"
               value="Chris Evans"
               v-model="selectedHorizontal"
+              :customclass="'w-[18px] h-[18px] rounded-full inline-block transition-all duration-200 ease-in-out'"
+
             />
 
              <RadioButton
@@ -53,6 +69,8 @@
             label="Liam Neeson"
             value="Liam Neeson"
             v-model="selectedHorizontal"
+            :customclass="'w-[18px] h-[18px] rounded-full inline-block transition-all duration-200 ease-in-out'"
+
           />
 
 
@@ -61,8 +79,19 @@
             label="Daniel Craig"
             value="Daniel Craig"
             v-model="selectedHorizontal"
+            :customclass="'w-[18px] h-[18px] rounded-full inline-block transition-all duration-200 ease-in-out'"
+
+          />
+           <RadioButton
+            name="paymentHorizontal"
+            label="Samafale"
+            value="Samafale"
+            v-model="selectedHorizontal"
+            :disabled="true"
+            :customclass="'cursor-not-allowed w-[18px] h-[18px] rounded-full inline-block transition-all duration-200 ease-in-out'"
           />
 
+          
           </div>
           <p class="p-2">
         The "Radio" component is a fundamental input element that allows users
@@ -85,8 +114,8 @@ export default {
   components: { RadioButton },
   data() {
     return {
-       selectedVertical: "",  
-      selectedHorizontal: ""
+      selectedVertical: "Chris Evans",  
+      selectedHorizontal: "Chris Evans"
     };
   },
 };
